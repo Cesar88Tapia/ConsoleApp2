3bb6318afbe84f81f5daffc7b78ad34e94dee301
César Tapia <c.tapia@korvi.net>
2018-10-05 16:39:02 -0500

    Finished types of classes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,21 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            //este es un comentario que quiero subir. 
+            int i = 0;
+            decimal x = 0.0m;
+            float f = 0.0f;
+            double d = 0.0D;
+            string cadena = "Hello world";
+            bool bandera = false;
+            DateTime fecha = DateTime.MinValue;
+            Console.WriteLine("The value of i is: {0}", i);
+            Console.WriteLine("The value of x is: {0:c}", x);
+            Console.WriteLine("The value of f is: {0:F2}", f);
+            Console.WriteLine("The value of d is: {0:F2}", d);
+            Console.WriteLine("The value of cadena is: " + cadena);
+            Console.WriteLine("The value of bandera is: " + bandera.ToString());
+            Console.WriteLine("The value of fecha is: " + fecha.ToShortDateString());
+            Console.ReadKey();
         }
     }
 }

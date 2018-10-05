a7bf769605a8f6f73947d0509c3ba2d599f433c4
César Tapia <c.tapia@korvi.net>
2018-10-05 17:05:23 -0500

    Changing values between variables

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,20 +10,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int i = 0;
-            decimal x = 0.0m;
-            float f = 0.0f;
-            double d = 0.0D;
-            string cadena = "Hello world";
-            bool bandera = false;
-            DateTime fecha = DateTime.MinValue;
+            int i = 10;
+            decimal x = 12.2m;
+            bool flag = true;
+            string chain = "1245784";
+            DateTime date = DateTime.MinValue;
+            i = Convert.ToInt32(chain);
             Console.WriteLine("The value of i is: {0}", i);
-            Console.WriteLine("The value of x is: {0:c}", x);
-            Console.WriteLine("The value of f is: {0:F2}", f);
-            Console.WriteLine("The value of d is: {0:F2}", d);
-            Console.WriteLine("The value of cadena is: " + cadena);
-            Console.WriteLine("The value of bandera is: " + bandera.ToString());
-            Console.WriteLine("The value of fecha is: " + fecha.ToShortDateString());
+            Console.WriteLine("The value of x is: {0:C}", x);
+            Console.WriteLine("The value of flag is: " + flag.ToString());
+            Console.WriteLine("The value of chain: " + chain);
+            Console.WriteLine("Teh value of date: " + date.ToShortDateString());
             Console.ReadKey();
         }
     }
